5628b034c16e2e7c178d0c1c8c5cd8306e4c4b53
TobiasAZED <125610758+TobiasAZED@users.noreply.github.com>
2024-07-02 15:57:38 -0700

    docker stuff

---
+++ Docker/NetworkDrive/NetworkDriveDocker/test.py
@@ -0,0 +1,13 @@
+# simple script for testing docker
+import pandas as pd
+import pyodbc
+import numpy as np
+import os
+print(os.environ)
+
+
+print(115)
+
+with open(r"\\Asasprdvm01\acct\ACCOUNTABILITY\2024\Tobias\Misc\test.txt", 'r') as file:
+    s = file.read()
+print(s)
